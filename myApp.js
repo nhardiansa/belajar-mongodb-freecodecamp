215f435705d11e2899eb0ed3a28521303bf68918
Nabil Hardiansa <nhardiansa@gmail.com>
2021-11-23 11:16:10 +0800

    first commit

--- myApp.js
+++ myApp.js
@@ -1,4 +1,10 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+  newUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 
 let Person;
